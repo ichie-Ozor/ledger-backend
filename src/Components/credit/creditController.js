68736014e2f40a4cef78a9e33860a2f25d352afe
Ichie Ozor <simeon_mc2000@yahoo.com>
2024-05-14 10:29:36 +0100

    update

--- src/Components/credit/creditController.js
+++ src/Components/credit/creditController.js
@@ -6,15 +6,17 @@ import {
     deleteCreditService,
     getCreditsByCreditorIdService
 } from './creditServices.js'
-import { getStocksByIdService, editStocksService } from '../stock/stockServices.js';
+import { getStocksByIdService, editStocksService, deleteStockService } from '../stock/stockServices.js';
 import APIError from '../../utils/customError.js';
+import { get, Types } from 'mongoose';
+import { Stock } from '../../models/stockModel.js';
 
 export const createCredit = async(req, res, next) => {
-    console.log(req.body, "it here")
+    console.log(req.body, "credit req.body")
     const incomingData = req.body
     try {
     for(let i = 0; i < incomingData.length; i++){
-        console.log(incomingData[i], "see incoming data")
+        console.log(incomingData[i], "see incoming credit")
 
     const {businessId, creditorId, description, category, qty, rate, date} = incomingData[i];
     if (!businessId || !creditorId || !description || !category || !qty || !rate || !date) {
@@ -24,36 +26,45 @@ export const createCredit = async(req, res, next) => {
     
     //get the stock 
     const getStock = await getStocksByIdService(businessId)
-    console.log(getStock, businessId, "see stock")
+    console.log(getStock, businessId, "see stock/credit")
+
+
 
     //if the stock is less than the credit, return a response
-    if(getStock[0].category !== category && getStock[0].qty < qty){
-         res.status(200).json({
+    for(let i = 0; i < getStock.length; i++){
+    if(getStock[i].goods === description && getStock[i].qty < qty){
+            return res.status(200).json({
             success: true,
-            message: 'There is not enough items in the stock DB'
+            message: 'There is not enough items in the stock DB',
+            code: 100,
+            credit:incomingData[i] 
           })
-    }
-
-   //check if the stock has the item
-   if(getStock.category !== category || getStock.length === 0) {
-       res.status(200).json({
-       success: true,
-       message: 'There is no item in the stock DB'
-     })
-   }
+      }
+    
     //if the stock is greater than the credit, subtract it
-    if(getStock.qty >= qty) {
-        let stockDiff = getStock.qty - qty
-        const editedStock = {...getStock}
-        editedStock.qty = stockDiff
-        const updatedStock = await editStocksService(businessId, editedStock)
+    if(getStock[i].goods === description && getStock[i].qty >= qty) {
+        let new_stock =await Stock.findById(getStock[i]._id).exec()
+        new_stock.qty = getStock[i].qty - qty
+        console.log(new_stock.qty, "new stock")
 
-            res.status(201).json({
-            success: true,
-            message: 'Credit created successfully!',
-            creditor: updatedStock
-         })
+        // if the stock qty is = 0 remove it from the stock
+        if(new_stock.qty === 0){
+            console.log("before")
+            await Stock.findByIdAndDelete(new_stock._id)
+            return res.status(200).json({
+                success: true,
+                message: 'There is no more stock in the Database',
+              })
+        }
+        await new_stock.save()
+        //     res.status(201).json({
+        //     success: true,
+        //     message: 'Credit created successfully!',
+        //     // creditor: updatedStock
+        //  })
+      } 
     }
+
     //this will save the credit to the credit DB
     const newCredit = await createCreditService(incomingData[i])
      res.status(201).json({
